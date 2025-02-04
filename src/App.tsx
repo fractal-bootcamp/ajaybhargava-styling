26eeb4956845c14e64d1b378ed7c57a9ffe95e1a
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-04 17:17:58 -0500

    colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,58 @@
-import './App.css'
-import MessageThread from './components/MessageThread'
+import "./App.css";
+import MessageThread from "./components/MessageThread";
 
 const messages = [
-  {
-    id: 1,
-    username: "One",
-    userId: 1,
-    message: "I just completed my first 10K this morning, and I feel amazing. It was a bit of a struggle towards the end, but pushing through the last kilometer was so rewarding. Now, I'm enjoying a big breakfast to refuel. If anyone wants to join me for a run next week, let me know."
-  },
-  {
-    id: 2,
-    username: "One",
-    userId: 1,
-    message: "I'm also planning a weekend getaway to the mountains. I'm excited to get away from the city and enjoy some fresh air. I'm also bringing my running shoes along, so if anyone wants to join me for a run, let me know."
-  },
-  {
-    id: 3,
-    username: "Two",
-    userId: 2,
-    message: "That's awesome! I'm proud of you. I'm also hitting the gym today to work on my strength. I'll let you know if I'm up for a run next week."
-  },
-  {
-    id: 4,
-    username: "One",
-    userId: 1,
-    message: "Something salacious. Has come up in the news. I'm not sure what to make of it. I'm not sure if I should be worried. I'm not sure if I should be excited. I'm not sure if I should be anything. I'm just not sure."
-  },
-  {
-    id: 5,
-    username: "One",
-    userId: 2,
-    message: "Something salacious. Has come up in the news. I'm not sure what to make of it. I'm not sure if I should be worried. I'm not sure if I should be excited. I'm not sure if I should be anything. I'm just not sure."
-  },
-
-]
+	{
+		id: 1,
+		username: "One",
+		userId: 1,
+		message:
+			"I just completed my first 10K this morning, and I feel amazing. It was a bit of a struggle towards the end, but pushing through the last kilometer was so rewarding. Now, I'm enjoying a big breakfast to refuel. If anyone wants to join me for a run next week, let me know.",
+	},
+	{
+		id: 2,
+		username: "One",
+		userId: 1,
+		message:
+			"I'm also planning a weekend getaway to the mountains. I'm excited to get away from the city and enjoy some fresh air. I'm also bringing my running shoes along, so if anyone wants to join me for a run, let me know.",
+	},
+	{
+		id: 3,
+		username: "Two",
+		userId: 2,
+		message:
+			"That's awesome! I'm proud of you. I'm also hitting the gym today to work on my strength. I'll let you know if I'm up for a run next week.",
+	},
+	{
+		id: 4,
+		username: "One",
+		userId: 1,
+		message:
+			"Something salacious. Has come up in the news. I'm not sure what to make of it. I'm not sure if I should be worried. I'm not sure if I should be excited. I'm not sure if I should be anything. I'm just not sure.",
+	},
+	{
+		id: 5,
+		username: "One",
+		userId: 2,
+		message:
+			"Something salacious. Has come up in the news. I'm not sure what to make of it. I'm not sure if I should be worried. I'm not sure if I should be excited. I'm not sure if I should be anything. I'm just not sure.",
+	},
+	{
+		id: 6,
+		username: "One",
+		userId: 1,
+		message:
+			"Something salacious. Has come up in the news. I'm not sure what to make of it. I'm not sure if I should be worried. I'm not sure if I should be excited. I'm not sure if I should be anything. I'm just not sure.",
+	},
+];
 
 function App() {
-  return (
-    <div>
-      <div className='text-4xl py-4 text-start'>
-        Message Thread
-      </div>
-      <MessageThread messages={messages} />
-    </div>
-  )
+	return (
+		<div>
+			<div className="text-4xl py-4 text-start">Message Thread</div>
+			<MessageThread messages={messages} />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/components/MessageThread.tsx
+++ src/components/MessageThread.tsx
@@ -1,49 +1,92 @@
 type Message = {
-    id: number,
-    username: string,
-    userId: number,
-    message: string,
-}
+	id: number;
+	username: string;
+	userId: number;
+	message: string;
+};
 
 interface MessageProps {
-    msg: Message,
-    isConsecutive: boolean
+	msg: Message;
+	hasPriorMessage: boolean;
+	hasNextMessage: boolean;
 }
 
-function isConsecutive(msg: Message, idx: number, messages: Message[]){
-    if (idx === 0 || messages[idx - 1] === undefined) return false
-    return msg.userId === messages[idx - 1].userId
+function hasPriorMessage(msg: Message, idx: number, messages: Message[]) {
+	if (idx === 0 || messages[idx - 1] === undefined) return false;
+	return msg.userId === messages[idx - 1].userId;
 }
 
-
-function Message({msg, isConsecutive}: MessageProps){
-    return (
-        <div id="Message" className={`grow ${msg.userId === 2 ? 'bg-gray-100' : 'bg-blue-300'} border-1 p-4 text-left rounded-2xl`}>
-            {msg.message}
-        </div>
-    )
+// look into the array
+function hasNextMessage(msg: Message, idx: number, messages: Message[]) {
+	if (idx === messages.length || messages[idx + 1] === undefined) return false;
+	return msg.userId === messages[idx + 1].userId;
 }
 
-export default function MessageThread ({messages} : {messages : Message[]}) {
-    return (
-    <div>
-        {messages.map((msg, idx, msgs) => {
-            return (
-            <div key={msg.id} className="flex w-2xl py-1">
-                <div id="AvatarLeft" className="flex-none">
-                    <div className={`${ msg.userId === 1 && !(isConsecutive(msg, idx, msgs)) ? 'grow rounded-full border-1 size-14 flex items-center justify-center' : 'grow rounded-full size-14 flex items-center justify-center' }`} >
-                    </div>
-                </div>
-                {/* Set to flex-row-reverse if the message is from the same user */}
-                <div id="Columns" className={`flex ${msg.userId === 1 ? 'flex-row' : 'flex-row-reverse' } min-w-full pl-2 pr-2`}> 
-                    <Message msg={msg} isConsecutive={isConsecutive(msg, idx, msgs)} />
-                    <div id="Spacer" className="grow min-w-[100px]" />
-                </div>
-                <div id="AvatarRight" className="flex-none">
-                    <div className={`${ msg.userId === 2 && !(isConsecutive(msg, idx, msgs)) ? 'grow rounded-full border-1 size-14 flex items-center justify-center' : 'invisible' }`} />
-                </div>
-            </div>
-        )})}
-    </div>
-    )
+function getCornerStyles(
+	msg: Message,
+	hasPriorMessage: boolean,
+	hasNextMessage: boolean,
+) {
+	if (!hasPriorMessage && !hasNextMessage) {
+		return "border-rounded";
+	}
+	if (msg.userId === 1 && hasPriorMessage) {
+		return "border-tl-rounded";
+	}
+	if (msg.userId === 1 && hasPriorMessage) {
+		return "border-tl-rounded";
+	}
+	if (msg.userId === 2 && hasPriorMessage) {
+		return "border-tl-rounded";
+	}
+	if (msg.userId === 2 && hasPriorMessage) {
+		return "border-tl-rounded";
+	}
+}
+
+function Message({ msg, hasPriorMessage, hasNextMessage }: MessageProps) {
+	return (
+		<div key={msg.id} className="flex w-2xl py-1">
+			<div id="AvatarLeft" className="flex-none">
+				<div
+					className={`${msg.userId === 1 && !hasPriorMessage ? "grow rounded-full border-1 size-14 flex items-center justify-center" : "grow rounded-full size-14 flex items-center justify-center"}`}
+				/>
+			</div>
+			{/* Set to flex-row-reverse if the message is from the same user */}
+			<div
+				id="Columns"
+				className={`flex ${msg.userId === 1 ? "flex-row" : "flex-row-reverse"} min-w-full pl-2 pr-2`}
+			>
+				<div
+					id="Message"
+					className={`grow ${msg.userId === 2 ? "bg-gray-100" : "bg-blue-300"} border-1 p-4 text-left rounded-2xl`}
+				>
+					{msg.message}
+				</div>
+				<div id="Spacer" className="grow min-w-[100px]" />
+			</div>
+			<div id="AvatarRight" className="flex-none">
+				<div
+					className={`${msg.userId === 2 && !hasPriorMessage ? "grow rounded-full border-1 size-14 flex items-center justify-center" : "invisible"}`}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default function MessageThread({ messages }: { messages: Message[] }) {
+	return (
+		<div>
+			{messages.map((msg, idx, msgs) => {
+				return (
+					<Message
+						key={msg.id}
+						msg={msg}
+						hasPriorMessage={hasPriorMessage(msg, idx, msgs)}
+						hasNextMessage={hasNextMessage(msg, idx, msgs)}
+					/>
+				);
+			})}
+		</div>
+	);
 }
