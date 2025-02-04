2247c74d978b30705dafbffdd03a5a96ab00bd78
Ajay Bhargava <bhargava.ajay@gmail.com>
2025-02-04 18:46:54 -0500

    instagram story complete

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Instagram Story
 
 This is a stateful instagram story component that passes avatar image, image, count, and description.
+
+
+```typescript
+
+<button type="button" onClick={() => incrementCounter(story.id)}>
+    Count: {counter[story.id]}
+</button>
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import './App.css'
-import InstagramStory from './components/InstagramStory'
+import "./App.css";
+import InstagramStory from "./components/InstagramStory";
 
-const instagram_story = [
-  {
-    id: 1,
-    avatar: './images/avatar.png',
-    image: './images/flowers.jpeg',
-    name: 'Helena',
-    group: 'Group name',
-    time: '3 min ago'
-  }
-]
+const stories = [
+	{
+		id: 1,
+		avatar: "./images/avatar.png",
+		image: "./images/flowers.jpeg",
+		name: "Helena",
+		group: "Group name",
+		time: "3 min ago",
+	},
+	{
+		id: 2,
+		avatar: "./images/avatar.png",
+		image: "./images/flowers.jpeg",
+		name: "Helena",
+		group: "Group name",
+		time: "3 min ago",
+	},
+];
 
 function App() {
-  return (
-    <div>
-      <div className='text-4xl py-4 text-start'>
-        Post
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<div className="text-4xl py-4 text-start">Post</div>
+			<InstagramStory stories={stories} />
+		</div>
+	);
 }
 
-export default App
+export default App;
